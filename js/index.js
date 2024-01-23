9c08d8742deca44ad392107697c68328f541ecbb
brahimbensalah <59453559+brahimbensalah@users.noreply.github.com>
2024-01-23 11:29:18 +0100

    done

--- js/index.js
+++ js/index.js
@@ -386,6 +386,7 @@ $(document).ready(function(){
      $('#rentcars').click(function(){
         var id=  $('#id-car').val();
         console.log(id);
+        alert('your car has rented')
         //garage.carRented(id)
      })
    /*****************************Add cars ********************************************* */ 
@@ -397,49 +398,7 @@ $('#addcars').on('click',function(){
    var price = $("#Price").val();
    var location = $("#location").val();
    garage.addCars(model,type,color,location,state,price)
-   $('.car').empty()
-   for(var i=0;i<=array.length-1;i++)
-   {
-     for(key in array[i])
-     {
-
-       $('.car').append(`<div class="item-car">
-       <div class="content-car">
-
-           <div class="profile-image-container" >
-               <img src="./photos/car-8.jpg" alt="" style="width: 100%;height: 100%;border-radius:20px;">
-           </div>
-
-           <div class="car-name">
-               <span class="text-name-car">${array[i].model}</span><hr>
-           </div>
-           <div class="car-description">
-               <div class="item-description" > </div>
-               <div class="item-description">text</div>
-               <div class="item-description">text</div>
-               <div class="item-description">text</div>
-               <div class="item-description">text</div>
-               <div class="item-description">text</div>
-           </div>
-           <div class="price" >
-               <div style="float: left;position: relative;top:10px;left: 10px;">
-               ${array[i].location}
-               </div>
-               <div style="float:right;position:relative;top:10px;right: 10px;">
-               ${array[i].price}
-               </div>
-           </div>
-           <div class="Rent">
-               <button class="button Rent-botton" id="rentcars">
-                   Rent Now
-               </button>
-
-           </div>
-
-       </div>
-   </div>`)
-     }
-   } 
+   alert('your cart has addet')
 })
    /******************************Delete cars************************************************************ */
 
